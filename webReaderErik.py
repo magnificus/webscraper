4a0fc75f1c512bbe19671bb33d9ac2f087538fd0
tobbe <tobiaselinder@gmail.com>
2017-02-14 14:21:18 +0100

    FÄRDIGT

--- webReaderErik.py
+++ webReaderErik.py
@@ -11,29 +11,31 @@ class Hit:
     	self.link = link
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-}
+    'User-Agent': 'Mozilla/5 (Solaris 10) Gecko'}
 
-response = requests.get("http://www.google.se/search?&tbm=nws&q=" + sys.argv[1], headers=headers)
-response.raise_for_status()
 
-soup = BeautifulSoup(response.text, "lxml")
-
-data = soup.findAll('div',attrs={'class':'g'})
-
+prevRes = 10
+curr = 0
 links = []
-for div in data:
-	first = re.search("<span class=\"f\">.*-.*</span></div><div class=\"st\"", str(div)).group(0)
-	second = re.search("-.*</s", first).group(0)
-	third = second[2:]
-	fourth = third[:-3]
-	#print("TOPRINT:" + fourth)
-	#print("DIV: " + str(div))
-	links.append(Hit(fourth, div.find('a')['href']));
-	#for link in div.findAll('a'):
-	#	links.append(link.get('href'));
+while (prevRes == 10):
+	response = requests.get("http://www.google.se/search?&tbm=nws&q=" + sys.argv[1] + "&start=" + (str(curr)), headers = headers)
+	response.raise_for_status()
+
+	soup = BeautifulSoup(response.text, "lxml")
+	data = soup.findAll('div',attrs={'class':'g'})
+
+	for div in data:
+		#print("DIV " + str(div))
+		desc = div.find('div', attrs={'class': 'st'}).text
+		name = div.find('a').text
+		link = div.find('a')['href'][7:]
+		second = div.find('span', attrs={'class' : 'f'}).text
+		links.append(Hit(second, name, link))
+	prevRes = len(data)
+	curr += prevRes
 
 for link in links:
-    print("\n" + link.name + " " + link.date)
+    print((link.name + "; " + link.date + "; " + link.link).encode('utf-8'))
 
 print (str(len(links)) + " results");
+print ("Data displayed as: Title; Date; Link")
